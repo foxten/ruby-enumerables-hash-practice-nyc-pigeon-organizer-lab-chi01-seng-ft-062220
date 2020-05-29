93063e63ff0f595f55771c1f337b2a5246ab081e
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-29 14:01:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  flat = []
+    data.each_key do |key|
+      flat << data[key].values
+    end
+    names = flat.flatten.uniq
+      names.each do |element|
+        result[element] = {}
+      end
+result
 end
